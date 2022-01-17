1000d6c0373a82af1df6cf6a3e55ae468d38cd6d
Alexandre Mathieu <alexandre.mathieu.7@ulaval.ca>
2022-01-16 23:51:46 -0500

    Fix burger menu when links are clicked

--- burger.js
+++ burger.js
@@ -10,3 +10,15 @@ burger.addEventListener("click", () => {
         burger.classList.add("active");
     }
 });
+
+// MAKE SURE THE MENU CLOSES WHEN YOU CLICK ON AN ELEMENT IN IT
+const nav__links = document.querySelectorAll(".nav__link");
+
+nav__links.forEach((nav__link) =>
+    nav__link.addEventListener("click", () => {
+        if (burger.classList.contains("active")) {
+            menu.classList.remove("burgerActive");
+            burger.classList.remove("active");
+        }
+    })
+);
